dd385f6be3dbba6c9fa5883ee5d922fbe7a7a72a
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-10-14 22:43:32 +0200

    Fixed bits and bobs.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ const SplitByPathPlugin = require('webpack-split-by-path');
 const TypedocPlugin = require('typedoc-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 const plugins = [
     new TypedocPlugin({
@@ -79,7 +80,7 @@ const loaders = [
     {
         test: /\.json$/,
         loader: 'json',
-    }
+    },
 ].concat([/\.svg$/, /\.eot$/, /\.woff2?$/, /\.ttf$/].map((pattern) => ({
     test: pattern,
     loader: 'url',
