06b27be30ea34f0a4e2d08ad1ab5895109b5e4a5
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-10-19 14:16:24 +0200

    Removed unused file.

--- src/styles.d.ts
+++
@@ -1,5 +0,0 @@
-declare var require: {
-    (path: string): any; // tslint:disable-line
-    (paths: string[], callback: (...modules: any[]) => void): void; // tslint:disable-line
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
